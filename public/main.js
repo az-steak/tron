d038146cabb305bf4f84cace49e1b402d7b7411b
Maxence Deschamps <azsteak36@gmail.com>
2017-04-27 10:34:52 +0200

    main

--- public/main.js
+++ public/main.js
@@ -2,14 +2,34 @@ require(['Controller'], function (Controller) {
 
     var socket;
     var controller;
+    var canvas;
+    var context;
 
     socket = new io();
     controller = new Controller($('body'));
+
+    canvas  = $(".whiteboard")[0];
+    context = canvas.getContext('2d');
+
+
+    /// Listeners
+
     setInterval(controllsCheck, 10);
 
+    socket.on("drawLine",drawLine);
 
     function controllsCheck () {
-        
+
+    }
+
+    function drawLine (data) {
+        context.beginPath();
+
+        context.arc(data.endPos[0], data.endPos[1], data.width, 0, 2 * Math.PI, false);
+        context.fillStyle = data.color;
+        context.fill();
+
+        context.closePath();
     }
 
 });
